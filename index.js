2b1a29a3b47e89443880eca11d6eba9f4ba92e00
bey <bey.liu@outlook.com>
2020-06-18 00:20:05 +0800

    feat(create): create script command.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// nothing now.
+#!/usr/bin/env node
+const program = require('commander')
+const figlet = require('figlet')
+const chalk = require('chalk')
+const cloneFromGithub = require('download-git-repo')
+
+program.version(require('./package.json').version)
+
+program
+  .command('create <name>')
+  .description('create your project')
+  .action((name) => {
+    try {
+      console.log('start to clone...')
+      cloneFromGithub('bey6/athena.git', 'src', { clone: true }, (err) => {
+        if (err) return Promise.reject(err)
+        else return Promise.resolve('success')
+      })
+    } catch (error) {
+      return Promise.reject(error)
+    } finally {
+      console.log('finished.')
+    }
+  })
+
+console.log(
+  chalk.greenBright(
+    figlet.textSync(
+      'Demeter',
+      {
+        font: 'Larry 3D 2',
+        horizontalLayout: 'default',
+        verticalLayout: 'default',
+      },
+      (err, data) => {
+        if (err) {
+          console.dir(err)
+          return
+        }
+        console.log(data)
+      }
+    )
+  )
+)
+console.log(process.argv)
+program.program.parse(process.argv)
